120a2934b0c334a8ddedbdabd2e444d657926b15
Rômulo Schiavon <schiavonromulo@gmail.com>
2019-11-29 17:24:23 -0300

    First trial, implementating useEffect

--- CrytopcurrencyApp/App.js
+++ CrytopcurrencyApp/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [apiData, setApiData] = useState(data);
+
+  useEffect(() => {
+    async function fetchData(){
+      const data = await fetch('https://api.coinmarketcap.com/vl/ticker/?limit=10');
+      const res = await data.json();
+      setApiData = (res);
+      console.log('res', res);
+    }
+    fetchData();
+  });
+
+  //https://api.coinmarketcap.com/vl/ticker/?limit=10 API
+
+  const renderItem = (item) => {
+    return (
+      <View>
+        <Text>Symbol: {item.symbol}</Text>
+        <Text>Price: {item.price}</Text>
+        <Text>Name: {item.name}</Text>
+      </View>
+
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Workshop @FotonTech @EJEC</Text>
+      <FlatList 
+        data={apiData}
+        renderItem={({ item }) => renderItem(item)}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
@@ -12,6 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
